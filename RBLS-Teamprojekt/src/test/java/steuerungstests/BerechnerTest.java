845a00604fb84535969c1cd612c484350724d86a
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-01-20 19:46:45 +0100

    Create BerechnerTest.java

---
+++ RBLS-Teamprojekt/src/test/java/steuerungstests/BerechnerTest.java
@@ -0,0 +1,14 @@
+package steuerungstests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BerechnerTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
