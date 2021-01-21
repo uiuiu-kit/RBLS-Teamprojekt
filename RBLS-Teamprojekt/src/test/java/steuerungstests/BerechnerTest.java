af98ec71012c5f2056b89776534bd0de52c59806
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-01-21 11:33:15 +0100

    Berechner korrigiert und Test geschrieben
    zwei neue Tests in BerechnerTest um die Funktionsweise von Berechner zu testen. Fehler in Berechner gefunden und behoben

--- RBLS-Teamprojekt/src/test/java/steuerungstests/BerechnerTest.java
+++ RBLS-Teamprojekt/src/test/java/steuerungstests/BerechnerTest.java
@@ -3,12 +3,26 @@ package steuerungstests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import steuerung.Berechner;
 
 public class BerechnerTest {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void berechnerTest2() {
+    int anzAtom = 2;
+    boolean[][] expFaelle = { { true, true }, { false, true }, { true, false }, { false, false } };
+    boolean[][] faelle = new boolean[4][2];
+    faelle = Berechner.faelleBerechnen(anzAtom, faelle, 0);
+    assertArrayEquals(expFaelle, faelle);
+  }
+
+  @Test
+  public void berechnerTest1() {
+    int anzAtom = 1;
+    boolean[][] expFaelle = { { true }, { false } };
+    boolean[][] faelle = new boolean[2][1];
+    faelle = Berechner.faelleBerechnen(anzAtom, faelle, 0);
+    assertArrayEquals(expFaelle, faelle);
   }
 
 }
