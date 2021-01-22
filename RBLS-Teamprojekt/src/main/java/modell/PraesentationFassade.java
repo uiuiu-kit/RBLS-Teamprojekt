7004f5cdafdf5ed8300fe7ed1c27c307d7e580ea
Florian G <horst.mustermann.dumm@gmail.com>
2021-01-22 20:24:20 +0100

    Checkstyle
    Hab gerade den Google-Checkstyle geklont und dann die Stern-Import Meldung rausgeworfen. Nur der neue Checkstyle wird noch nichtübernommen..

--- RBLS-Teamprojekt/src/main/java/modell/PraesentationFassade.java
+++ RBLS-Teamprojekt/src/main/java/modell/PraesentationFassade.java
@@ -1,5 +1,17 @@
 package modell;
 
+import java.util.List;
+
+import modell.formel.*;
+
 public class PraesentationFassade {
 
+  public void init() {
+    //TODO
+  }
+  
+  public List<Formel> gibAtomareAussage() {
+    //TODO
+    return null;
+  }
 }
