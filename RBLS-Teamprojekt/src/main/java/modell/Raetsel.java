8353091d8cfe3407c57c5bf813624531eed429c8
ucqtr <77849492+ucqtr@users.noreply.github.com>
2021-01-23 15:18:09 +0100

    Create Raetsel.java
    Klasse Raetsel hinzugefuegt.

---
+++ RBLS-Teamprojekt/src/main/java/modell/Raetsel.java
@@ -0,0 +1,21 @@
+package modell;
+
+import java.util.List;
+
+import modell.formel.Atom;
+import modell.formel.Formel;
+
+public class Raetsel {
+  
+  private String raetselText;
+  private int stufe;
+  private List<Atom> atom;
+  private String antworttext;
+  private List<String> antworten;
+  private List<Formel> formeln;
+
+  public Raetsel() {
+    
+  }
+  
+}
