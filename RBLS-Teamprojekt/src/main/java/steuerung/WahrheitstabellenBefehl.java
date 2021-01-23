3a2af3a5bb43377725c7c3d6d3868a0986364e5a
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-01-23 17:39:31 +0100

    Create WahrheitstabellenBefehl.java

---
+++ RBLS-Teamprojekt/src/main/java/steuerung/WahrheitstabellenBefehl.java
@@ -0,0 +1,31 @@
+package steuerung;
+
+import modell.SteuerungFassade;
+
+public abstract class WahrheitstabellenBefehl {
+  SteuerungFassade model;
+
+  /**
+   * Konstruktor für WahrheitstabellenBefehl.
+   * 
+   * @param model die Fassde von der Daten geholt und bei der Daten gesetzt
+   *              werden.
+   */
+  public WahrheitstabellenBefehl(SteuerungFassade model) {
+    this.model = model;
+  }
+
+  /**
+   * hohlt alle Daten die für den Befehl benötigt werden von der Fassade.
+   */
+  public void hohleDaten() {
+
+  }
+
+  /**
+   * Stezt in der Fassade alle Daten die der Befehl verändert hat.
+   */
+  public void setzeDaten() {
+
+  }
+}
