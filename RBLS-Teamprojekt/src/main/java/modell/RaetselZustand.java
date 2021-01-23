3699ab30db49f1176eb0c893cfc451211a80ea8b
ucqtr <77849492+ucqtr@users.noreply.github.com>
2021-01-23 14:02:05 +0100

    Create RaetselZustand.java
    Klasse RaetselZustand hinzugefuegt

---
+++ RBLS-Teamprojekt/src/main/java/modell/RaetselZustand.java
@@ -0,0 +1,15 @@
+package modell;
+
+/**
+ * 
+ * @author Janne Wagner
+ * @version 1.0
+ * 
+ * Diese Klasse beinhaltet den Zustand des bestimmten Raetsels sowie seinen Namen.
+ *
+ */
+public class RaetselZustand {
+
+  public String raetselname;
+  public boolean geloest;
+}
