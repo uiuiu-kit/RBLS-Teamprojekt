e216c8ee87dd6a5cf54568f159dded20402682fa
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-01-23 17:39:35 +0100

    Create AufbauTabelle.java

---
+++ RBLS-Teamprojekt/src/main/java/steuerung/AufbauTabelle.java
@@ -0,0 +1,28 @@
+package steuerung;
+
+import java.util.List;
+import modell.SteuerungFassade;
+import modell.formel.Atom;
+
+public abstract class AufbauTabelle extends WahrheitstabellenBefehl {
+
+  List<Atom> atomareAussagen;
+
+  /**
+   * Konstruktor für den Befehle vom Typ AufbauTabelle.
+   * 
+   * @param model die Fassde von der Daten geholt und bei der Daten gesetzt
+   *              werden.
+   */
+  public AufbauTabelle(SteuerungFassade model) {
+    super(model);
+  }
+
+  /**
+   * Holt die Atomaren Aussagen von der Fassade da sie in jedem Befehl vom Typ
+   * AufbauTabelle verwendet werden.
+   */
+  private void holeDaten() {
+    atomareAussagen = model.gibAtomareAussagen();
+  }
+}
