65b0ea292a749117805087f630b6e42c733850d6
ucqtr <77849492+ucqtr@users.noreply.github.com>
2021-01-24 22:54:09 +0100

    Create Memento.java
    Klasse Memento erstellt

---
+++ RBLS-Teamprojekt/src/main/java/modell/raetsel/Memento.java
@@ -0,0 +1,22 @@
+package modell.raetsel;
+
+public class Memento {
+
+  public void sichern(Raetsel r) {
+    //TODO
+  }
+  
+  public RaetselZustand gibSicherung() {
+    //TODO
+    return null;
+  }
+  
+  public void loesche(String s) {
+    //TODO
+  }
+  
+  public String leseSicherung() {
+    //TODO
+    return null;
+  }
+}
