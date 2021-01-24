7d9a5cfa384e00ae4104f82686e9ca132926621d
ucqtr <77849492+ucqtr@users.noreply.github.com>
2021-01-24 22:54:24 +0100

    Create Raetselinterpret.java
    Klasse Raetselinterpret erstellt

---
+++ RBLS-Teamprojekt/src/main/java/modell/raetsel/Raetselinterpret.java
@@ -0,0 +1,24 @@
+package modell.raetsel;
+
+import java.util.List;
+
+public class Raetselinterpret {
+
+  public List<String> liesOrdner(int i) {
+    //TODO
+    return null;
+  }
+  
+  public Raetsel liesRaetsel(String name) {
+    //TODO
+    return null;
+  }
+  
+  public void erstelleFR(List<String> atomA) {
+    //TODO
+  }
+  
+  public void erstelleRaetsel() {
+    //TODO
+  }
+}
