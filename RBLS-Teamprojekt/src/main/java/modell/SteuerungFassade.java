bbdc574c6826a652609570f03380b7d9d8208f3b
ucqtr <77849492+ucqtr@users.noreply.github.com>
2021-01-24 11:47:28 +0100

    Update SteuerungFassade.java
    Methoden in Fassade Steuerung

--- RBLS-Teamprojekt/src/main/java/modell/SteuerungFassade.java
+++ RBLS-Teamprojekt/src/main/java/modell/SteuerungFassade.java
@@ -1,5 +1,74 @@
 package modell;
 
-public class SteuerungFassade {
+import java.util.List;
+import modell.formel.Atom;
+import modell.formel.Formel;
+import modell.tabelle.Tabelle;
 
+public class SteuerungFassade {
+  
+  private Tabelle tabelle = new Tabelle(0, 0, 0);
+
+  public void init() {
+    //TODO
+  }
+  
+  public List<Atom> gibAtomareAussage() {
+    //TODO
+    return null;
+  }
+  
+  public List<String> gibNoetigeFormel() {
+    //TODO
+    return null;
+  }
+  
+  public boolean gibZelleWW(int[] i) {
+    return tabelle.gibZellenWert(i);
+  }
+  
+  public void setzeZelleWW(int[] i) {
+    //TODO
+  }
+  
+  public int gibZeilenAnz() {
+    return tabelle.gibZeilenAnz();
+  }
+  
+  public int gibSpaltenAnz() {
+    return tabelle.gibSpaltenAnz();
+  }
+  
+  public boolean gibZeileFall(int zeile) {
+    return tabelle.gibZeileFall(zeile);
+  }
+  
+  public void spalteHinzufuegen() {
+    tabelle.spalteHinzufuegen();
+  }
+  
+  public void spalteEntfernen(int spalte) {
+    tabelle.spalteEntfernen(spalte);
+  }
+  
+  public void fuehreSicherungAus() {
+    //TODO
+  }
+  
+  public Formel gibFormel(int spalte) {
+    return tabelle.gibFormel(spalte);
+  }
+  
+  public void setzeFormel(Formel f, int spalte) {
+    //TODO
+  }
+  
+  public String gibFormelText(int spalte) {
+    //TODO
+    return null;
+  }
+  
+  public void setzeAktRaetsel(String raetselname) {
+    //TODO
+  }
 }
