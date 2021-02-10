4f13acaed06c79d12ca041b923843458ef2f6c3f
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-02-10 09:49:09 +0100

    Update FormelEingeben.java
    zellenKoordinate durch spalte ausgetauscht

--- RBLS-Teamprojekt/src/main/java/steuerung/FormelEingeben.java
+++ RBLS-Teamprojekt/src/main/java/steuerung/FormelEingeben.java
@@ -7,20 +7,19 @@ import modell.formel.Formel;
 
 public class FormelEingeben extends WahrheitstabellenBefehl {
 
-  private int[] zellenKoordinaten;
+  private int spalte;
   private String alteFromel;
   private List<Atom> atomareAussagen;
 
   /**
    * Der Konstruktor, für die Befehl, der den Befehl auch direkt ausführt.
    * 
-   * @param model             die Fassade auf die der Befehl zugreift.
-   * @param zellenKoordinaten die Koordinaten der Zelle dessen Formel geändert
-   *                          werden soll.
+   * @param model  die Fassade auf die der Befehl zugreift.
+   * @param spalte die Spalte in der die Formel geändert werden soll.
    */
-  public FormelEingeben(SteuerungFassade model, int[] zellenKoordinaten) {
+  public FormelEingeben(SteuerungFassade model, int spalte) {
     super(model);
-    this.zellenKoordinaten = zellenKoordinaten;
+    this.spalte = spalte;
     hohleDaten();
     setzeDaten();
   }
@@ -29,18 +28,18 @@ public class FormelEingeben extends WahrheitstabellenBefehl {
    * hohlt die alte Formel und die atomaren Aussagen von der Fasssade.
    */
   public void hohleDaten() {
-    alteFromel = model.gibFormelText(zellenKoordinaten[0]);
+    alteFromel = model.gibFormelText(spalte);
     atomareAussagen = model.gibAtomareAussage();
   }
 
   /**
-   * Ruf das Dialogfenster auf, in dem man die Formel editieren kann, und überträgt
-   * die neue Formel, über die Fassade, zum Modell.
+   * Ruf das Dialogfenster auf, in dem man die Formel editieren kann, und
+   * überträgt die neue Formel, über die Fassade, zum Modell.
    */
   public void setzeDaten() {
     FormelEditor fe = new FormelEditor(atomareAussagen);
     String neueFormel = fe.gibNeueFormel(alteFromel);
     Formel neueFormelF = FormelParser.pars(neueFormel, model);
-    model.setzeFormel(neueFormelF, zellenKoordinaten[0]);
+    model.setzeFormel(neueFormelF, spalte);
   }
 }
