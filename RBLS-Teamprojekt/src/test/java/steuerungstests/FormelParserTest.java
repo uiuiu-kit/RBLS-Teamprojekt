3a91e2398684431bb3f5c4b94bfc4e1cf1212fcd
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-02-13 14:05:30 +0100

    Klammer
    Wer hätte gedacht das es noch nicht fertig ist??
    Ja war es aber jetzt hoffentlich ; )

--- RBLS-Teamprojekt/src/test/java/steuerungstests/FormelParserTest.java
+++ RBLS-Teamprojekt/src/test/java/steuerungstests/FormelParserTest.java
@@ -17,6 +17,7 @@ public class FormelParserTest {
 
   @Mock
   private SteuerungFassade sfMock;
+  private boolean[] fall = new boolean[3];
 
   /**
    * initalisiert die SteuerungFassade und die atomaen Aussagen.
@@ -27,6 +28,9 @@ public class FormelParserTest {
     atomareAussagenMock.add("Charlie");
     atomareAussagenMock.add("Donald");
     atomareAussagenMock.add("Edgar");
+    fall[0] = true;
+    fall[1] = false;
+    fall[2] = true;
     sfMock = Mockito.mock(SteuerungFassade.class);
     Mockito.when(sfMock.gibAtomareAussage()).thenReturn(atomareAussagenMock);
   }
@@ -34,53 +38,70 @@ public class FormelParserTest {
   @Test
   public void testEinAtome() {
     Formel formel = FormelParser.pars("0", sfMock);
+    assertEquals(true, formel.auswerten(fall));
     assertEquals("C", formel.gibStringRep());
   }
 
   @Test
   public void testZweiAtome() {
     Formel formel = FormelParser.pars("1u2", sfMock);
+    assertEquals(false, formel.auswerten(fall));
     assertEquals("DuE", formel.gibStringRep());
+  }
+
+  @Test
+  public void testXor() {
+    Formel formel = FormelParser.pars("1x2", sfMock);
+    assertEquals(true, formel.auswerten(fall));
+    assertEquals("DxE", formel.gibStringRep());
   }
 
   @Test
   public void testDreiAtom() {
     Formel formel = FormelParser.pars("1u2o0", sfMock);
+    assertEquals(true, formel.auswerten(fall));
     assertEquals("DuEoC", formel.gibStringRep());
   }
 
   @Test
   public void testNurKlammer() {
     Formel formel = FormelParser.pars("(1)", sfMock);
+    assertEquals(false, formel.auswerten(fall));
     assertEquals("D", formel.gibStringRep());
   }
-  
+
   @Test
   public void testNicht() {
     Formel formel = FormelParser.pars("n1", sfMock);
+    assertEquals(true, formel.auswerten(fall));
     assertEquals("nD", formel.gibStringRep());
   }
 
   @Test
   public void testKlammerMitKonnektor() {
     Formel formel = FormelParser.pars("(1u2)", sfMock);
+    assertEquals(false, formel.auswerten(fall));
     assertEquals("DuE", formel.gibStringRep());
   }
 
   @Test
   public void testKlammerOderZusatz() {
     Formel formel = FormelParser.pars("0o(1u2)", sfMock);
-    assertEquals("CoDuE", formel.gibStringRep());   
+    assertEquals(true, formel.auswerten(fall));
+    assertEquals("CoDuE", formel.gibStringRep());
   }
-  
+
   @Test
   public void testKlammerUndZusatz() {
-    Formel formel = FormelParser.pars("0u(0x0)", sfMock);
-    assertEquals("CuCxC", formel.gibStringRep());
+    Formel formel = FormelParser.pars("0u(1x2)", sfMock);
+    assertEquals(true, formel.auswerten(fall));
+    assertEquals("CuDxE", formel.gibStringRep());
   }
-  
+
+  @Test
   public void testdoppelKlammer() {
-    Formel formel = FormelParser.pars("0u(0x(0o0))", sfMock);
-    assertEquals("CuCxCo0", formel.gibStringRep());
+    Formel formel = FormelParser.pars("0u(2x(1o1))", sfMock);
+    assertEquals(true, formel.auswerten(fall));
+    assertEquals("CuExDoD", formel.gibStringRep());
   }
 }
