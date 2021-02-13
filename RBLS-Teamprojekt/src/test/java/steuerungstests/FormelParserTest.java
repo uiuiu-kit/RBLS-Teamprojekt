0033b2a24551cf1b8f5c70182e5352cb792f0558
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-02-13 15:29:14 +0100

    Update FormelParserTest.java
    unnötoge Imports entfernt

--- RBLS-Teamprojekt/src/test/java/steuerungstests/FormelParserTest.java
+++ RBLS-Teamprojekt/src/test/java/steuerungstests/FormelParserTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 import modell.SteuerungFassade;
-import modell.formel.Atom;
 import modell.formel.Formel;
 import org.junit.Before;
 import org.junit.Test;
