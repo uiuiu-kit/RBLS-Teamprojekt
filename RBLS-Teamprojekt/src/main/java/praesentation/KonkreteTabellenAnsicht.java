4ba2a2fb9ef5880ae77272e3c642f667d613df02
unhfb <77682098+unhfb@users.noreply.github.com>
2021-02-14 15:39:24 +0100

    Update KonkreteTabellenAnsicht.java
    mehr Kommentare entfernt

--- RBLS-Teamprojekt/src/main/java/praesentation/KonkreteTabellenAnsicht.java
+++ RBLS-Teamprojekt/src/main/java/praesentation/KonkreteTabellenAnsicht.java
@@ -60,8 +60,8 @@ public class KonkreteTabellenAnsicht extends TabellenAnsicht {
   private void init() {
 
     //TODO unkommentieren wenn fertig
-    //zeilenzahl = modell.gibZeilenAnz();
-    //spaltenzahl = modell.gibSpaltenAnz();
+    zeilenzahl = modell.gibZeilenAnz();
+    spaltenzahl = modell.gibSpaltenAnz();
     //stufe = modell.gibAktuelleStufe();
     markierteZeilen = new boolean[zeilenzahl];
     Arrays.fill(markierteZeilen, false);
