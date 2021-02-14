a40b9b03b9c11b2866cd6d7fce274c1fc0839c61
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-02-14 22:01:35 +0100

    atom list
    String list zu Atom list Umwandler gebaut

--- RBLS-Teamprojekt/src/main/java/modell/raetsel/Raetsel.java
+++ RBLS-Teamprojekt/src/main/java/modell/raetsel/Raetsel.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import modell.formel.Atom;
 
-/**Das Raetsel ist ein Objekt um die Daten der Raetseltextdatei 
- * dem Rest des Programms zur Verfuegung zu stellen.
- * Es wird daher vom Raetselinterpreten erstellt und der Fassade uebergeben.
+/**
+ * Das Raetsel ist ein Objekt um die Daten der Raetseltextdatei dem Rest des
+ * Programms zur Verfuegung zu stellen. Es wird daher vom Raetselinterpreten
+ * erstellt und der Fassade uebergeben.
+ * 
  * @author Flo
  *
  */
 public class Raetsel {
-  
+
   protected String raetselText;
   protected int stufe;
   protected List<Atom> atom;
@@ -23,7 +25,7 @@ public class Raetsel {
   protected int zeilenAnz;
   protected int loesung;
   protected String name;
-  
+
   /**
    * @param name
    * @param stufe
@@ -34,11 +36,12 @@ public class Raetsel {
    * @param antworttext
    * @param formeln
    */
-  public Raetsel(String name, int stufe, List<String> atom, String raetselText, List<String> antwortMöglichkeiten, int loesung, String antworttext, List<String> formeln) {
+  public Raetsel(String name, int stufe, List<String> atom, String raetselText, List<String> antwortMöglichkeiten,
+      int loesung, String antworttext, List<String> formeln) {
     this.spaltenAnz = atom.size();
     this.zeilenAnz = (int) Math.pow(2, atom.size());
     this.stufe = stufe;
-    this.atom = new ArrayList<Atom>();
+    stringToAtomList(atom);
     this.loesung = loesung;
     this.raetselText = raetselText;
     this.formeln = formeln;
@@ -46,21 +49,30 @@ public class Raetsel {
     this.name = name;
     this.antworten = antwortMöglichkeiten;
   }
-  
+
+  private void stringToAtomList(List<String> atomS) {
+    atom = new ArrayList<Atom>();
+    for (int i = 0; i < atomS.size(); i++) {
+      atom.add(new Atom(atomS.get(i), i));
+    }
+  }
+
   public String gibRaetselText() {
     return raetselText;
   }
-  
+
   public int gibStufe() {
     return stufe;
   }
-  
+
   public String gibName() {
     return this.name;
   }
-  
-  /**Wandelt die Liste der Atome in eine Liste der entsprechenden Namen der Atome um 
-   * und gibt diese zurueck.
+
+  /**
+   * Wandelt die Liste der Atome in eine Liste der entsprechenden Namen der Atome
+   * um und gibt diese zurueck.
+   * 
    * @return Liste der Atomnamen.
    */
   public List<String> gibAtomNamen() {
@@ -70,47 +82,50 @@ public class Raetsel {
     }
     return temp;
   }
-  
+
   public List<String> gibAtomareAussage() {
-    List<String> output = new ArrayList<String>();   
+    List<String> output = new ArrayList<String>();
     for (Atom temp : atom) {
       output.add(temp.gibStringRep());
     }
     return output;
   }
-  
+
   public String gibAntworttext() {
     return antworttext;
   }
-  
+
   public List<String> gibAntwort() {
     return antworten;
   }
-  
+
   public String gibLoesung() {
     return this.antworten.get(loesung - 1);
   }
-  
-  /**Wandelt die Liste der Formeln in eine Liste der entsprechenden Namen der Formeln um 
-   * und gibt diese zurueck.
-   * @return Liste der benotigten Formelnamen, die zur Loesung des Raetsels benoetigt werden.
+
+  /**
+   * Wandelt die Liste der Formeln in eine Liste der entsprechenden Namen der
+   * Formeln um und gibt diese zurueck.
+   * 
+   * @return Liste der benotigten Formelnamen, die zur Loesung des Raetsels
+   *         benoetigt werden.
    */
   public List<String> gibFormeln() {
     return formeln;
   }
-  
+
   public int gibSpaltenAnz() {
     return this.spaltenAnz;
   }
-  
+
   public int gibZeilenAnz() {
     return this.zeilenAnz;
   }
-  
+
   public int gibAtomAnz() {
     return atom.size();
   }
-  
+
   public List<Atom> gibAtome() {
     return atom;
   }
