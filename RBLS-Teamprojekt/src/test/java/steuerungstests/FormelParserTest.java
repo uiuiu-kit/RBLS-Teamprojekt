97ee6fc5f73d3db51c062c00fbe4db2820de24fe
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-02-15 00:33:09 +0100

    maven update

--- RBLS-Teamprojekt/src/test/java/steuerungstests/FormelParserTest.java
+++ RBLS-Teamprojekt/src/test/java/steuerungstests/FormelParserTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import modell.Fassade;
 import modell.formel.Formel;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
