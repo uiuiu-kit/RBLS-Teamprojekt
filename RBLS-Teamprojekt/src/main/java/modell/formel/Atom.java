fbe5b49a210d30a07b880c282417edf7c50d4287
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-03-03 17:41:00 +0100

    Issue #48 behoben
    Issue #48 behoben und Zahlenwerte durch Buchstaben ausgetauscht.

--- RBLS-Teamprojekt/src/main/java/modell/formel/Atom.java
+++ RBLS-Teamprojekt/src/main/java/modell/formel/Atom.java
@@ -1,11 +1,11 @@
 package modell.formel;
 
-/** Das Atom enthaelt den Wahrheitswert der atomaren Aussage 
- * und den Namen des repraesentierten Subjekts. 
- * Es erbt direkt von der Formel, ist also kein Konnektor 
- * und stellt damit das Ende jeder auswerten()-Kette dar.
- * Es wird als Großbuchstabe in der Formel dargestellt.
-
+/**
+ * Das Atom enthaelt den Wahrheitswert der atomaren Aussage und den Namen des
+ * repraesentierten Subjekts. Es erbt direkt von der Formel, ist also kein
+ * Konnektor und stellt damit das Ende jeder auswerten()-Kette dar. Es wird als
+ * Großbuchstabe in der Formel dargestellt.
+ * 
  * @author Flo
  *
  */
@@ -14,18 +14,19 @@ public class Atom extends Formel {
   private String aussage;
   private String rep;
   private int nummer;
-    
-  /** Konstruktor. Setzt nur seine Werte, die er uebergeben bekommt.
 
+  /**
+   * Konstruktor. Setzt nur seine Werte, die er uebergeben bekommt.
+   * 
    * @param aussage Entspricht dem vollen Namen des Atoms (zB Herbert).
-   * @param nummer Position im Werte-Array.
+   * @param nummer  Position im Werte-Array.
    */
   public Atom(String aussage, int nummer) {
-    this.aussage = aussage + "(" + nummer + ")";
+    this.aussage = aussage + "(" + (char) (65 + nummer) + ")";
     if (nummer == -1) {
       this.rep = "Formel einfügen";
     } else {
-      this.rep = "" + nummer;
+      this.rep = "" + (char) (65 + nummer);
     }
     this.nummer = nummer;
     this.bindungsstaerke = 6;
