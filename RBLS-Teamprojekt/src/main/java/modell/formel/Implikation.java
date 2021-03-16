9d8f665777a57d0b1c660b63c4be9823440afbfb
uiuiu-kit <77333987+uiuiu-kit@users.noreply.github.com>
2021-03-16 19:05:40 +0100

    Update Implikation.java
    Implikation zeigt nun auch Klammern bei anderen Implikationenn an

--- RBLS-Teamprojekt/src/main/java/modell/formel/Implikation.java
+++ RBLS-Teamprojekt/src/main/java/modell/formel/Implikation.java
@@ -1,18 +1,20 @@
 package modell.formel;
 
-/**Unterklasse des Bikonnektors.
- * Impliziert logisch aus dem linken Formelnachbarn den rechten Formelnachbarn.
- * Seine String Repraesentation ist "f".
-
+/**
+ * Unterklasse des Bikonnektors. Impliziert logisch aus dem linken
+ * Formelnachbarn den rechten Formelnachbarn. Seine String Repraesentation ist
+ * "f".
+ * 
  * @author Flo
  *
  */
 public class Implikation extends BiKonnektor {
 
-  /** Kostruktor. Setzt uebergebene Werte und seine String Rep.
-
+  /**
+   * Kostruktor. Setzt uebergebene Werte und seine String Rep.
+   * 
    * @param rechts Die rechte Formel.
-   * @param links Die linke Formel.
+   * @param links  Die linke Formel.
    */
   public Implikation(Formel links, Formel rechts) {
     this.rechts = rechts;
@@ -21,7 +23,7 @@ public class Implikation extends BiKonnektor {
     this.bindungsstaerke = 1;
     this.zeichen = "f";
   }
-  
+
   @Override
   public boolean auswerten(boolean[] werte) {
     if (links.auswerten(werte) == true && rechts.auswerten(werte) == false) {
@@ -29,4 +31,39 @@ public class Implikation extends BiKonnektor {
     }
     return true;
   }
+
+  @Override
+  public String gibStringRep() {
+    String ausdruck;
+    ausdruck = this.rep;
+    if (bindungsstaerke >= links.bindungsstaerke) {
+      ausdruck = "(" + links.gibStringRep() + ")" + ausdruck;
+    } else {
+      ausdruck = links.gibStringRep() + ausdruck;
+    }
+    if (bindungsstaerke >= rechts.bindungsstaerke) {
+      ausdruck = ausdruck + "(" + rechts.gibStringRep() + ")";
+    } else {
+      ausdruck = ausdruck + rechts.gibStringRep();
+    }
+    return ausdruck;
+  }
+
+  @Override
+  public String gibParsable() {
+    String ausdruck;
+    ausdruck = this.zeichen;
+    if (bindungsstaerke >= links.bindungsstaerke) {
+      ausdruck = "(" + links.gibParsable() + ")" + ausdruck;
+    } else {
+      ausdruck = links.gibParsable() + ausdruck;
+    }
+    if (bindungsstaerke >= rechts.bindungsstaerke) {
+      ausdruck = ausdruck + "(" + rechts.gibParsable() + ")";
+    } else {
+      ausdruck = ausdruck + rechts.gibParsable();
+    }
+    return ausdruck;
+  }
+
 }
